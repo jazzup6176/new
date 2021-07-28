3c752c543a020e6405fba1443f58da97beb56ec5
jazzup6176 <88086548+jazzup6176@users.noreply.github.com>
2021-07-28 12:10:53 +0200

    Add files via upload

---
+++ 15. Ethereum-nicehash.bat
@@ -0,0 +1,3 @@
+PhoenixMiner.exe -pool stratum+tcp://daggerhashimoto.eu.nicehash.com:3353 -wal 3DNkNa1RoYuFJ9uKnTv6ZuSN2vyyCnnNEC -pass x -log 0 -retrydelay 1 -ftime 55 -tt 79 -tstop 89 -tstart 85 -Rmode 1 -proto 4 -stales 0 -fret 1 -rate 1
+pause
+
